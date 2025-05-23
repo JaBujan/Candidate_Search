69ea218c69c6cc71e3d9c89cf4c13027a0a49bd2
JaBujan <javierbcardenas97@gmail.com>
2025-05-22 22:56:12 -0400

    .

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';
-
-const Nav = () => {
-  return (
-    <div className="bg-blue-600 text-white shadow-md sticky top-0 z-50 h-screen w-64 fixed">
-      <nav className="flex flex-col p-6 space-y-4 items-start justify-start text-left">
-        <h1 className="text-2xl font-bold mb-6 text-left"></h1>
-        <ul className="flex flex-col items-start space-y-4 text-left">
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-200 transition duration-200 text-left"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/saved-candidates"
-              className="hover:text-blue-200 transition duration-200 text-left"
-            >
-              Potential Candidates
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-
-export default Nav;
-
-=======
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
@@ -40,4 +6,3 @@ const Nav = () => {
 };
 
 export default Nav;
->>>>>>> 030fa864e866ba716b7b3847e62f6991c3eac7a9
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,14 +1,1 @@
 // TODO: Create an interface for the Candidate objects returned by the API
-<<<<<<< HEAD
-export interface Candidate {
-    id: number; // Unique identifier for the candidate
-    name: string; // Full name of the candidate
-    username: string; // Candidate's username
-    location: string; // Candidate's location
-    avatar: string; // URL to the candidate's avatar image
-    email: string; // Candidate's email address
-    html_url: string; // Link to the candidate's GitHub profile
-    company: string; // Candidate's current company or organization
-  }
-=======
->>>>>>> 030fa864e866ba716b7b3847e62f6991c3eac7a9
