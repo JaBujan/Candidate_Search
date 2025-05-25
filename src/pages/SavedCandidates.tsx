d549ba7f096ac554bfb59c01c2d86071d137f3fe
JaBujan <javierbcardenas97@gmail.com>
2025-05-25 01:23:44 -0400

    page that render the candidates info on a table

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,36 @@
+import { useCandidate } from '../contexts/CandidateContext';
+import { Candidate } from '../interfaces/Candidate.interface';
+
 const SavedCandidates = () => {
+  const { savedCandidatesList } = useCandidate();
+
   return (
     <>
       <h1>Potential Candidates</h1>
+      {(savedCandidatesList.length <= 0) ? <h2> There are no candidates to display</h2> :
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Location</th>
+            <th>Email</th>
+            <th>Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          {savedCandidatesList.map((candidate: Candidate, index: number) => {
+            return <tr key={candidate.id || index}>
+            <td id='img-container'><img id='table-img' src={candidate.avatar}></img></td>
+            <td>{candidate.name || 'N/A'}</td>
+            <td>{candidate.username}</td>
+            <td>{candidate.location || 'None provided'}</td>
+            <td>{candidate.email || 'None provided'}</td>
+            <td>{candidate.company || 'None specified'}</td>
+          </tr>})}
+        </tbody>
+      </table>}
     </>
   );
 };
